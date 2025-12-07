3acc30cfb2e1304c33413b211cb09a84fc141fde
Naquadah125 <nhocktete01@gmail.com>
2025-12-08 04:29:36 +0700

    add 4 navigation tab

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Features from './pages/Features';
-import Partners from './pages/Partners';
-import Contact from './pages/Contact';
-import Quiz from './pages/Quiz';
 import './App.css';
 
 function App() {
@@ -13,10 +9,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/partners" element={<Partners />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </Router>
   );
